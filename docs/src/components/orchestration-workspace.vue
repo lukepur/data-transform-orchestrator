e633832b492f5a770b885352df5c3a279d963e80
Luke Purton <luke.purton@gmail.com>
2017-06-08 13:21:37 +1000

    Render d3 graph

--- docs/src/components/orchestration-workspace.vue
+++ docs/src/components/orchestration-workspace.vue
@@ -3,16 +3,17 @@
     @drop="onDrop"
     @dragenter="cancel"
     @dragover="cancel">
-    <svg :width="svg.width" :height="svg.height" :id="workspaceId"></svg>
+    <svg :width="svg.width" :height="svg.height" :id="svg.workspaceId"></svg>
   </div>
 </template>
 
 <script>
 import { find } from 'lodash';
-import d3 from 'd3';
 import transforms from '../orchestrations/transforms';
 import Orchestrator from 'data-transform-orchestrator';
 import { config } from '../orchestrations/ema-for-field';
+
+const d3 = require('d3');
 
 export default {
   name: 'orchestration-workspace',
@@ -43,19 +44,59 @@ export default {
       return new Orchestrator(this.orchestrationConfig);
     }
   },
-  created () {
-    const svg = d3.select('#'+this.svg.workspaceId);
+  mounted () {
+    const svg = d3.select('#' + this.svg.workspaceId);
     const { nodes, links } = this.orchestration.meta();
+    const { width, height } = this.svg;
+
+    // flatten links for d3 force()
+    const d3Links = links.map(link => ({
+      source: link.source.nodeId,
+      sourcePath: link.source.path,
+      target: link.target.nodeId,
+      targetPath: link.target.path
+    }));
+    const d3Nodes = [...nodes];
+    d3Nodes.push({id: 'userInput'});
 
     const simulation = d3.forceSimulation()
-      .force('link', d3.forceLink().id(d => d.id))
-      .force('charge', d3.forceManyBody())
-      .force('center', d3.forceCenter(this.svg.width / 2, this.svg.height / 2));
+          .force('link', d3.forceLink().id(d => d.id))
+          .force('charge', d3.forceManyBody()(-20))
+          .force('center', d3.forceCenter(width / 2, height / 2));
 
-    const link = svg.append('g')
-      .attr('class', 'links')
-    .selectAll('line')
-    .data(links)
+    const n = nodes.length;
+
+    simulation.nodes(d3Nodes).on('tick', ticked);
+    simulation.force('link').links(d3Links);
+
+    d3Nodes.forEach((d, i) => {
+      d.x = d.y = width / n * i;
+    });
+
+    const link = svg.selectAll('.link')
+          .data(d3Links)
+      .enter().append('line')
+        .attr('class', 'link')
+        .style('stroke-width', d => Math.sqrt(d.value));
+
+    const node = svg.selectAll('.node')
+          .data(d3Nodes)
+      .enter().append('circle')
+        .attr('class', 'node')
+        .attr('r', 4.5)
+        .attr('title', d => d.id)
+        .style('fill', '#ff0000');
+
+    function ticked () {
+      link
+        .attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y);
+      node
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y);
+    }
   }
 }
 </script>
@@ -66,5 +107,9 @@ export default {
   min-height: 600px;
   border: solid 1px #d3d3d3;
   padding: 1em;
+}
+
+.link {
+  stroke: #aaa;
 }
 </style>
