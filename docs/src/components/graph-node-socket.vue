56c5febcf39e9fe3cd76922ade222b5c2a382ae6
Luke Purton <luke.purton@gmail.com>
2017-06-09 18:17:42 +1000

    Continue on socket rendering

--- docs/src/components/graph-node-socket.vue
+++ docs/src/components/graph-node-socket.vue
@@ -1,34 +1,17 @@
 <template>
-  <circle :cx="x" :cy="y" r="10" class="socket" :class="socket.type">
+  <circle :cx="socket.point.x" :cy="socket.point.y" :r="width" class="socket" :class="socket.type">
   </circle>
 </template>
 
 <script>
-import { find, last } from 'lodash';
-
 export default {
   name: 'graph-node-socket',
   props: {
     parentNode: Object,
-    socket: Object // { target: 'path', type: 'source|target'}
+    socket: Object, // { taget: 'path', type: 'source|target', point: {x, y}}
+    width: Number
   },
-  computed: {
-    socketLink () {
-      return find(this.parentNode.links, link => link.metaLink[this.socket.type].path === this.socket.target);
-    },
-    x () {
-      if (this.socketLink) {
-        return this.socket.type === 'target' ? last(this.socketLink.points).x : this.socketLink.points[0].x;
-      }
-      return 0;
-    },
-    y () {
-      if (this.socketLink) {
-        return this.socket.type === 'target' ? last(this.socketLink.points).y : this.socketLink.points[0].y;
-      }
-      return 0;
-    }
-  }
+  computed: {}
 }
 </script>
 
